4e7bad319e5e4da2c0c97a3377c4f3a787902db4
YotaLibero <54599349+YotaLibero@users.noreply.github.com>
2022-02-14 14:17:57 +0800

    Update U_T.py

--- АСОИУ/U_T.py
+++ АСОИУ/U_T.py
@@ -1,15 +1,11 @@
 import unittest
 from Module_calc import Calculator
 
-
-# Test cases to test Calulator methods
-# You always create  a child class derived from unittest.TestCase
 class TestCalculator(unittest.TestCase):
-    # setUp method is overridden from the parent class TestCase
+    
     def setUp(self):
         self.calculator = Calculator()
-
-    # Each test method starts with the keyword test_
+    
     def test_add(self):
         self.assertEqual(self.calculator.add(4, 7), 11)
 
@@ -23,6 +19,5 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calculator.divide(10, 2), 5)
 
 
-# Executing the tests in the above test case class
 if __name__ == "__main__":
     unittest.main()
